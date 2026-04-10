84608841e342e4897f32523293be5b4efbd76c96
RafaelFerreiraNeves <82104589+RafaelFerreiraNeves@users.noreply.github.com>
2026-04-10 16:39:35 -0300

    Add files via upload

---
+++ main.tf
@@ -0,0 +1,62 @@
+module "vpc" {
+  source = "terraform-aws-modules/vpc/aws"
+
+  name = var.aws_vpc_name
+  cidr = var.aws_vpc_cidr
+
+  azs             = var.aws_vpc_azs
+  private_subnets = var.aws_vpc_private_subnets
+  public_subnets  = var.aws_vpc_public_subnets
+
+  enable_nat_gateway = true
+  enable_vpn_gateway = true
+
+  tags = merge(var.aws_project_tags, { "kubernetes.io/cluster/${var.aws_eks_name}" = "shared" })
+
+
+
+  public_subnet_tags = {
+    "kubernetes.io/cluster/${var.aws_eks_name}" = "shared"
+    "kubernetes.io/role/elb"                    = 1
+  }
+  private_subnet_tags = {
+    "kubernetes.io/cluster/${var.aws_eks_name}" = "shared"
+    "kubernetes.io/role/internal-elb"           = 1
+  }
+}
+
+module "eks" {
+  source  = "terraform-aws-modules/eks/aws"
+  version = "~> 21.0"
+
+  name               = var.aws_eks_name
+  kubernetes_version = var.aws_eks_version
+
+  # Optional
+  endpoint_public_access = true
+
+  # Optional: Adds the current caller identity as an administrator via cluster access entry
+  enable_cluster_creator_admin_permissions = true
+
+  compute_config = {
+    enabled    = true
+    node_pools = ["general-purpose"]
+  }
+
+  vpc_id     = "vpc-1module.vpc.vpc_id"
+  subnet_ids = ["module.vpc.private_subnets"]
+
+
+  eks_managed_node_groups = {
+    default = {
+      instance_types = var.aws_eks_managed_node_groups_instance_types
+      desired_size   = 2
+      max_size       = 3
+      min_size       = 1
+    }
+  }
+
+
+  tags = var.aws_project_tags
+
+}
