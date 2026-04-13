838516fd574020fa9a254f54508fad49eb5ba700
RafaelFerreiraNeves <82104589+RafaelFerreiraNeves@users.noreply.github.com>
2026-04-13 18:55:51 -0300

    Update providers.tf

--- src/providers.tf
+++ src/providers.tf
@@ -7,7 +7,7 @@ terraform {
   }
 
   backend "s3" {
-    bucket = "rafaelferreiranevesprojetoterraform"
+    bucket = "terraformbucketprojetorafaelferreiraneves"
     key    = "env/dev/terraform.tfstate"
     region = "us-east-1"
   }
