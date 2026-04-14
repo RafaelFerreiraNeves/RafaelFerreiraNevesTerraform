4fec3607ac384f31cf68c749bb5519a9f686b69e
RafaelFerreiraNeves <82104589+RafaelFerreiraNeves@users.noreply.github.com>
2026-04-14 17:47:10 -0300

    Update main.tf

--- src/main.tf
+++ src/main.tf
@@ -1,42 +1,11 @@
-module "vpc" {
-  source = "terraform-aws-modules/vpc/aws"
-
-  name = var.aws_vpc_name
-  cidr = var.aws_vpc_cidr
-
-  azs             = var.aws_vpc_azs
-  private_subnets = var.aws_vpc_private_subnets
-  public_subnets  = var.aws_vpc_public_subnets
-
-  enable_nat_gateway = true
-  single_nat_gateway = true
-  enable_vpn_gateway = true
-
-  tags = merge(
-    var.aws_project_tags,
-    { "kubernetes.io/cluster/${var.aws_eks_name}" = "shared" }
-  )
-
-  public_subnet_tags = {
-    "kubernetes.io/cluster/${var.aws_eks_name}" = "shared"
-    "kubernetes.io/role/elb"                    = 1
-  }
-
-  private_subnet_tags = {
-    "kubernetes.io/cluster/${var.aws_eks_name}" = "shared"
-    "kubernetes.io/role/internal-elb"           = 1
-  }
-}
-
 module "eks" {
   source  = "terraform-aws-modules/eks/aws"
   version = "~> 21.0"
 
-  name               = var.aws_eks_name
-  kubernetes_version = var.aws_eks_version
+  cluster_name    = var.aws_eks_name
+  cluster_version = var.aws_eks_version
 
-  endpoint_public_access = true
-  cluster_endpoint_public_access_cidrs = ["0.0.0.0/0"]
+  cluster_endpoint_public_access = true
 
   enable_cluster_creator_admin_permissions = true
 
